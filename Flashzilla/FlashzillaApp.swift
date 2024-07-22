690c625a1b24256127e367a21c04e613fe1e771f
Om Preetham <ompreethambandi@gmail.com>
2024-07-22 11:50:47 -0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Om Preetham Bandi on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
